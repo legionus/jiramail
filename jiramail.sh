ebba50db5d24fe767d509bc3eb6c8bcd1ae3de1d
Alexey Gladkov <gladkov.alexey@gmail.com>
2023-09-11 11:51:04 +0200

    Remove unused code
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- jiramail.sh
+++ jiramail.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #
-# Run b4 from a git checkout.
+# Run jiramail from a git checkout.
 #
 
 REAL_SCRIPT=$(realpath -e ${BASH_SOURCE[0]})
 SCRIPT_TOP="${SCRIPT_TOP:-$(dirname ${REAL_SCRIPT})}"
 
-exec env PYTHONPATH="${SCRIPT_TOP}:${SCRIPT_TOP}/patatt" python3 "${SCRIPT_TOP}/jiramail/command.py" "${@}"
+exec env PYTHONPATH="${SCRIPT_TOP}" python3 "${SCRIPT_TOP}/jiramail/command.py" "${@}"
